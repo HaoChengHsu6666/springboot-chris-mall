2b687a4df06a22ff7f686790917cd82129083529
Chris <becomerocker@gmail.com>
2022-03-22 16:12:07 +0800

    初始化 Spring Boot project

---
+++ src/main/java/com/chrishsu/springbootchrismall/SpringbootChrisMallApplication.java
@@ -0,0 +1,13 @@
+package com.chrishsu.springbootchrismall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootChrisMallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootChrisMallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chrishsu/springbootchrismall/SpringbootChrisMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chrishsu.springbootchrismall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootChrisMallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
