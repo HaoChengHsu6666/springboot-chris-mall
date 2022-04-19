c614b090db22112f9e467639ec2279219f0d575f
Chris <becomerocker@gmail.com>
2022-04-19 19:13:15 +0800

    商品功能 : 實作查詢商品列表(分頁)

--- src/main/java/com/chrishsu/springbootchrismall/controller/ProductController.java
+++ src/main/java/com/chrishsu/springbootchrismall/controller/ProductController.java
@@ -9,11 +9,15 @@ import com.chrishsu.springbootchrismall.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import java.util.List;
 
+@Validated //為了使@Max() @Min()生效
 @RestController
 public class ProductController {
 
@@ -22,13 +26,17 @@ public class ProductController {
 
     @GetMapping("/products")
     public ResponseEntity<List<Product>> getProducts(
-            // 查詢條件 Filtering
+            // 查詢條件 Filtering  //沒傳值就是null
             @RequestParam(required = false ) ProductCategory category,
             @RequestParam(required = false) String search,
 
             //排序 Sorting
             @RequestParam(defaultValue = "created_date") String orderBy,
-            @RequestParam(defaultValue = "desc") String sort
+            @RequestParam(defaultValue = "desc") String sort,
+
+            //分頁 Pagination
+            @RequestParam(defaultValue = "5") @Max(1000) @Min(0) Integer limit,
+            @RequestParam(defaultValue = "0") @Min(0) Integer offset
     ){
 
         ProductQueryParams productQueryParams = new ProductQueryParams();
@@ -36,6 +44,8 @@ public class ProductController {
         productQueryParams.setSearch(search);
         productQueryParams.setOrderBy(orderBy);
         productQueryParams.setSort(sort);
+        productQueryParams.setLimit(limit);
+        productQueryParams.setOffset(offset);
 
         List<Product> productList = productService.getProducts(productQueryParams);
 
